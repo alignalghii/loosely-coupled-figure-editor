1a4c7ac0593417f0868e3edec3d5181b1eb0349a
Endrey Márk <ungazik@gmail.com>
2019-04-13 15:50:16 +0200

    Grasping is handled well, no need to use Figure reference point for that

--- App.js
+++ App.js
@@ -8,31 +8,31 @@ function App(svgLowLevel, coordSysTransformer, bijectionUp, originFigure)
 
 App.prototype.run = function ()
 {
-	var [prevEl, prevPos] = [null, null];
+	var [prevEl, prevPos, virgin] = [null, null, true];
 	var app = this;
 	this.svgLowLevel.subscribe(
 		'mousedown',
-		function (              [x,y]) {prevEl = prevPos = null;},
-		function (polygonChild, [x,y]) {prevEl = polygonChild; prevPos = [x, y];}
+		function (           currentPos) {prevEl = prevPos = null;                  virgin = true;},
+		function (currentEl, currentPos) {prevEl = currentEl; prevPos = currentPos; virgin = true;}
 	);
 	this.svgLowLevel.subscribe(
 		'mousemove',
-		function (              [x,y]) {if (prevEl) app.updateWidgetPillarFromLow(prevEl, [x,y]);},
-		function (polygonChild, [x,y]) {if (prevEl) app.updateWidgetPillarFromLow(prevEl, [x,y]);}
+		function (           currentPos) {if (prevEl) {app.updateWidgetPillarFromLow(prevEl, prevPos, currentPos); prevPos = currentPos; virgin = false;}},
+		function (currentEl, currentPos) {if (prevEl) {app.updateWidgetPillarFromLow(prevEl, prevPos, currentPos); prevPos = currentPos; virgin = false;}}
 	);
 	this.svgLowLevel.subscribe(
 		'mouseup',
-		function (              [x,y])
-		{	if ( prevEl) app.updateWidgetPillarFromLow(prevEl, [x,y]);
-			if (!prevEl) app.createWidgetPillarFromLow(        [x,y]);
-			prevEl = prevPos = null;
+		function (              currentPos)
+		{	if ( prevEl) app.updateWidgetPillarFromLow(prevEl, prevPos, currentPos);
+			if (!prevEl) app.createWidgetPillarFromLow(                 currentPos);
+			prevEl = prevPos = null; virgin = true;
 		},
-		function (polygonChild, [x,y])
+		function (currentEl, currentPos)
 		{
-			if ( prevEl                           && !Eq.eq([x, y], prevPos)) app.updateWidgetPillarFromLow(prevEl      , [x,y]);
-			if ( prevEl && polygonChild == prevEl &&  Eq.eq([x, y], prevPos)) app.deleteWidgetPillarFromLow(polygonChild       );
-			if (!prevEl                                                     ) app.createWidgetPillarFromLow(              [x,y]);
-			prevEl = prevPos = null;
+			if ( prevEl                        && !virgin) app.updateWidgetPillarFromLow(prevEl   , prevPos, currentPos);
+			if ( prevEl && currentEl == prevEl &&  virgin) app.deleteWidgetPillarFromLow(currentEl                     );
+			if (!prevEl                                  ) app.createWidgetPillarFromLow(                    currentPos);
+			prevEl = prevPos = null; virgin = true;
 		}
 	);
 
@@ -49,14 +49,16 @@ App.prototype.createWidgetPillarFromLow = function (svgPosition)
 	this.bijectionUp.set(svgNewPolygonCild, geomNewFigure);
 };
 
-App.prototype.updateWidgetPillarFromLow = function (lowElem, svgPosition)
+App.prototype.updateWidgetPillarFromLow = function (lowElem, svgPosition1, svgPosition2)
 {
-	var geomFigure       = this.bijectionUp.get(lowElem);
-	var geomDestPosition = this.coordSysTransformer.lowToHigh(svgPosition);
-	var geomDisplacement = fromTo(geomFigure.grasp, geomDestPosition);
+	var geomFigure        = this.bijectionUp.get(lowElem);
+	var geomDestPosition1 = this.coordSysTransformer.lowToHigh(svgPosition1);
+	var geomDestPosition2 = this.coordSysTransformer.lowToHigh(svgPosition2);
+	var geomDisplacement  = fromTo(geomDestPosition1, geomDestPosition2);
 	geomFigure.doTranslation(geomDisplacement);
 	var svgVertices = geomFigure.vertices.map((p) => this.coordSysTransformer.highToLow(p));
 	this.svgLowLevel.updatePolygonChild(lowElem, svgVertices);
+	//svgPosition1 = svgPosition2;
 };
 
 App.prototype.deleteWidgetPillarFromLow = function (lowElem)
