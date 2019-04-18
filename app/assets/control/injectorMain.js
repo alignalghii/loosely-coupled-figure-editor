82cad7d845eb91145271e876beece5fa732aba1d
Endrey Márk <ungazik@gmail.com>
2019-04-18 10:03:22 +0200

    Directory structure

--- app/assets/control/injectorMain.js
+++ app/assets/control/injectorMain.js
@@ -15,7 +15,7 @@ onload = function (event)
 
 	var board               = bijectionUp; // when using bijectionUp as a set of high-level figures, we call it a board
 	var originFigure        = bank.namedFigures[bank.selected].figure;
-	var audio               = new MyAudio(new Audio('sonar.ogg'));
+	var audio               = new MyAudio(new Audio('assets/sonar.ogg'));
 
 	var app                 = new App(board, originFigure, audio, widgetFactory);
 
