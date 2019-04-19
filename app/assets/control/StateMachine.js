e584baa2ce66260f18a2e4f42bbb06c867b62dcd
Endrey Márk <ungazik@gmail.com>
2019-04-19 11:43:23 +0200

    Simpilfying the state machine a little, keeping  the same behavior

--- app/assets/control/StateMachine.js
+++ app/assets/control/StateMachine.js
@@ -14,24 +14,22 @@ StateMachine.prototype.transition = function (eventType, inputSignature, ird) //
 {
 	switch (eventType) {
 		case 'mousedown':
-			if (this.onEmptySpace(inputSignature))
-				this.forgetAll();
+			this.forgetAll();
 			if (this.onAWidget(inputSignature)) {
-				this.rememberWidget(ird); this.rememberPosition(ird);
-				this.dragHasAlreadyBegun  = false;
-				this.hasCollided = false;
+				this.rememberWidget(ird);
+				this.rememberPosition(ird);
 				ird.currentWidget.showGlittering();
 			}
 		break;
 		case 'mousemove':
 			if (this.prevWidgetHasNotCollidedYet()) {
-				this.checkCollision(ird);
+				this.followWhileCheckCollision(ird);
 				this.rememberPosition(ird);
 				this.dragHasAlreadyBegun = true;
 			}
 		break;
 		case 'mouseup':
-			if (this.onAWidget(inputSignature) && this.prevWidgetHasNotCollidedYet() && this.samePlaceUpAsDown(ird) && !this.dragHasAlreadyBegun)
+			if (this.onAWidget(inputSignature) && this.prevWidgetHasNotCollidedYet() && !this.dragHasAlreadyBegun)
 				ird.currentWidget.delete();
 			if (this.prevWidgetHasNotCollidedYet() && this.dragHasAlreadyBegun) {
 				this.prevWidget.update(this.prevWEPos, ird.currentWEPos);
@@ -46,16 +44,18 @@ StateMachine.prototype.transition = function (eventType, inputSignature, ird) //
 
 /** Actions */
 
-StateMachine.prototype.rememberWidget   = function (ird) {this.prevWidget = ird.currentWidget;};
-StateMachine.prototype.rememberPosition = function (ird) {this.prevWEPos = ird.currentWEPos;};
-StateMachine.prototype.checkCollision   = function (ird) {this.hasCollided = this.app.checkAndHandleCollision(this.prevWidget, this.prevWEPos, ird.currentWEPos);};
+StateMachine.prototype.rememberWidget            = function (ird) {this.prevWidget = ird.currentWidget;};
+StateMachine.prototype.rememberPosition          = function (ird) {this.prevWEPos = ird.currentWEPos;};
+StateMachine.prototype.followWhileCheckCollision = function (ird) {this.hasCollided = this.app.checkAndHandleCollision(this.prevWidget, this.prevWEPos, ird.currentWEPos);};
 
 /** Conditions */
 
-StateMachine.prototype.prevWidgetHasNotCollidedYet = function () {return this.prevWidget && !this.hasCollided;};
+StateMachine.prototype.prevWidgetHasNotCollidedYet = function (   ) {return this.prevWidget && !this.hasCollided;};
 StateMachine.prototype.samePlaceUpAsDown           = function (ird) {return ird.currentWidget.high == this.prevWidget.high;};
-StateMachine.prototype.prevWidgetHasNotCollidedYet = function () {return this.prevWidget && !this.hasCollided;};
-StateMachine.prototype.samePlaceUpAsDown           = function (ird) {return ird.currentWidget.high == this.prevWidget.high;};
-
-StateMachine.prototype.onEmptySpace = function (inputSignature) {return Eq.eq(inputSignature, ['WEPos']);};
+StateMachine.prototype.prevWidgetHasNotCollidedYet = function (   ) {return this.prevWidget && !this.hasCollided;};
 StateMachine.prototype.onAWidget    = function (inputSignature) {return Eq.eq(inputSignature, ['Widget', 'WEPos']);};
+
+/** Not needed */
+
+//StateMachine.prototype.samePlaceUpAsDown           = function (ird) {return ird.currentWidget.high == this.prevWidget.high;};
+//StateMachine.prototype.onEmptySpace = function (inputSignature) {return Eq.eq(inputSignature, ['WEPos']);};
