46d98c5301a697df4a5d5ee6a1121ee1d85bd0df
Endrey Márk <ungazik@gmail.com>
2020-05-06 18:09:20 +0200

    `CanvasMultiplicityComponent` merged into `ControllerMixingCanvasWidgetable`

--- public/app/controllers/ControllerMixinCanvasWidgetable.js
+++ public/app/controllers/ControllerMixinCanvasWidgetable.js
@@ -11,3 +11,15 @@ const ControllerMixinCanvasWidgetable =
 		return widget_;
 	}
 };
+
+// `CanvasMultiplicityComponent` merged into `ControllerMixingCanvasWidgetable` // @TODO
+// CakePHP 3.8 Red Velvet Cookbook: ``Components are packages of logic that are shared between controllers'' @link https://book.cakephp.org/3.0/en/controllers/components.html
+const selectCanvasPseudoWidgetForCanvas = (canvas, canvasPseudoWidgets) => unsafeFind(factorsOn(canvas), canvasPseudoWidgets),
+      factorsOn                         = canvas  =>  canvasPseudoWidget => canvasPseudoWidget.low() == canvas; // @TODO very rude arbitrariness
+
+const canvasOfEitherTarget = eitherTarget =>
+	either(
+		canvas        => canvas,
+		currentWidget => currentWidget.low.parentNode,
+		eitherTarget
+	);
