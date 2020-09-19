89f0d24ace64623512b0b10a3e304644a0f4c53b
Endrey Márk <ungazik@gmail.com>
2020-09-19 08:14:30 +0200

    floor-plan-designer: Move `MagnetController`'s helper `BiFigCxt` to other 2-figure context modules

--- floor-plan-designer/public/app.js/controllers/MagnetController.js
+++ floor-plan-designer/public/app.js/controllers/MagnetController.js
@@ -98,12 +98,3 @@ MagnetController.prototype.getMaybeBiFigCxt = function (currentWEPos, eitherTarg
 		)
 	);
 };
-
-// Alternative names: FigureBetweennessContext
-function BiFigCxt(polygon1, polygon2, maybeNearestEdgeFromPolygon1, widget1) // @TODO DRY redundancy
-{
-	this.polygon1 = polygon1;
-	this.polygon2 = polygon2;
-	this.maybeNearestEdgeFromPolygon1 = maybeNearestEdgeFromPolygon1;
-	this.widget1 = widget1; // @TODO DRY redundancy
-}
---
+++ floor-plan-designer/public/app.js/gravity/BiFigCxt.js
@@ -0,0 +1,8 @@
+// Alternative names: FigureBetweennessContext
+function BiFigCxt(polygon1, polygon2, maybeNearestEdgeFromPolygon1, widget1) // @TODO DRY redundancy
+{
+	this.polygon1 = polygon1;
+	this.polygon2 = polygon2;
+	this.maybeNearestEdgeFromPolygon1 = maybeNearestEdgeFromPolygon1;
+	this.widget1 = widget1; // @TODO DRY redundancy
+}
