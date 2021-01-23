971a82bedf6f6505f003cdece4d666a85e409e8a
Endrey Márk <ungazik@gmail.com>
2021-01-23 19:35:42 +0100

    floor-plan-designer: Figure edge and vertex editing can be set to be area-invariant, switchable on/off from config panel

--- floor-plan-designer/public/app.js/controllers/FigureEditorController.js
+++ floor-plan-designer/public/app.js/controllers/FigureEditorController.js
@@ -11,15 +11,15 @@ function FigureEditorController(state, canvasPseudoWidgets, statusBarDriver)
 Object.assign(FigureEditorController.prototype, ControllerMixinCanvasWidgetable );
 Object.assign(FigureEditorController.prototype, ControllerMixinHistoryFollowable);
 
-FigureEditorController.prototype.addVertex      = function (point, eitherTarget           ) {this.withProxFig('addVertex'     , point, eitherTarget, 'Hozzáadom'    , 'alakzathoz'           );};
-FigureEditorController.prototype.deleteVertex   = function (point, eitherTarget           ) {this.withProxFig('deleteVertex'  , point, eitherTarget, 'Elveszem'     , 'alakzatból'           );};
-FigureEditorController.prototype.moveVertex     = function (point, eitherTarget           ) {this.withProxFig('moveVertex'    , point, eitherTarget, 'Megmozdítom'  , 'alakzatnál'           );};
+FigureEditorController.prototype.addVertex      = function (point, eitherTarget           ) {this.withProxFig('addVertex'     , point, eitherTarget, 'Hozzáadom'    , 'alakzathoz', this.state.areaInvariance);};
+FigureEditorController.prototype.deleteVertex   = function (point, eitherTarget           ) {this.withProxFig('deleteVertex'  , point, eitherTarget, 'Elveszem'     , 'alakzatból', this.state.areaInvariance);};
+FigureEditorController.prototype.moveVertex     = function (point, eitherTarget           ) {this.withProxFig('moveVertex'    , point, eitherTarget, 'Megmozdítom'  , 'alakzatnál', this.state.areaInvariance);};
 
-FigureEditorController.prototype.pushEdge       = function (point, eitherTarget           ) {this.withProxFig('pushEdge'      , point, eitherTarget, 'Élt tolok'    , 'alakzatnál'           );};
-FigureEditorController.prototype.pushnormalEdge = function (point, eitherTarget           ) {this.withProxFig('pushnormalEdge', point, eitherTarget, 'Élt tolok'    , 'alakzatnál'           );};
-FigureEditorController.prototype.spanEdge       = function (point, eitherTarget, mouseDrag) {this.withProxFig('spanEdge'      , point, eitherTarget, 'Élt nyújtok'  , 'alakzatnál', mouseDrag);};
+FigureEditorController.prototype.pushEdge       = function (point, eitherTarget           ) {this.withProxFig('pushEdge'      , point, eitherTarget, 'Élt tolok'    , 'alakzatnál', this.state.areaInvariance);};
+FigureEditorController.prototype.pushnormalEdge = function (point, eitherTarget           ) {this.withProxFig('pushnormalEdge', point, eitherTarget, 'Élt tolok'    , 'alakzatnál', this.state.areaInvariance);};
+FigureEditorController.prototype.spanEdge       = function (point, eitherTarget, mouseDrag) {this.withProxFig('spanEdge'      , point, eitherTarget, 'Élt nyújtok'  , 'alakzatnál', mouseDrag, this.state.areaInvariance);};
 
-FigureEditorController.prototype.withProxFig = function (command, currentWEPos, eitherTarget, template1, template2, optArg) // @TODO: reuse: almost the same algorithm exists in `GeomTransforamtionController` (or in its section in `Router`).
+FigureEditorController.prototype.withProxFig = function (command, currentWEPos, eitherTarget, template1, template2, optArg, optArg2) // @TODO: reuse: almost the same algorithm exists in `GeomTransforamtionController` (or in its section in `Router`).
 {
 	this.locker.lockIfNecessary();
 
@@ -29,7 +29,8 @@ FigureEditorController.prototype.withProxFig = function (command, currentWEPos, 
 		'Nem határozható meg egyértelműen legközelebbi alakzat.',
 		nearestFigure => {
 			const figureEditor  = new FigureEditorByProximityHeuristic(nearestFigure);
-			optArg ? figureEditor[command](currentWEPos, optArg) : figureEditor[command](currentWEPos);
+			optArg2 == 'undefined' ? figureEditor.plainExec(command, currentWEPos, optArg)
+			                       : figureEditor[command](currentWEPos, optArg, optArg2);
 			const widget = canvasPseudoWidget.arbitrary.detectTypeAndComposeFromHigh(nearestFigure); // @TODO arbitrary
 			return widget.updateAndReport(currentWEPos, nearestFigure, template1, template2); // Image's boxing figure should not be allowed to be edited!
 		},
--- floor-plan-designer/public/app.js/math/haskell/app/FigureEditorByProximityHeuristic.js
+++ floor-plan-designer/public/app.js/math/haskell/app/FigureEditorByProximityHeuristic.js
@@ -3,25 +3,44 @@
 
 function FigureEditorByProximityHeuristic(figure) {this.figure = figure;}
 
-FigureEditorByProximityHeuristic.prototype.addVertex_rough    = function (point) {this.figure.vertices = addVertex   (this.figure.vertices, point);};
-FigureEditorByProximityHeuristic.prototype.deleteVertex_rough = function (point) {this.figure.vertices = deleteVertex(this.figure.vertices, point);};
-FigureEditorByProximityHeuristic.prototype.moveVertex_rough   = function (point) {this.figure.vertices = moveVertex  (this.figure.vertices, point);};
-
-FigureEditorByProximityHeuristic.prototype.pushEdge       = function (point)             {this.figure.vertices = pushEdge      (this.figure.vertices, point            );};
-FigureEditorByProximityHeuristic.prototype.pushnormalEdge = function (point)             {this.figure.vertices = pushnormalEdge(this.figure.vertices, point            );};
-FigureEditorByProximityHeuristic.prototype.spanEdge       = function (point, dragVector) {this.figure.vertices = spanEdge      (this.figure.vertices, point, dragVector);};
 
 
-FigureEditorByProximityHeuristic.prototype.withAreaInvariance = function (command, point)
+FigureEditorByProximityHeuristic.prototype.addVertex    = function (point, isAreaInvariant) {this.withAreaInvarianceIf('addVertex_rough'   , isAreaInvariant, point);};
+FigureEditorByProximityHeuristic.prototype.deleteVertex = function (point, isAreaInvariant) {this.withAreaInvarianceIf('deleteVertex_rough', isAreaInvariant, point);};
+FigureEditorByProximityHeuristic.prototype.moveVertex   = function (point, isAreaInvariant) {this.withAreaInvarianceIf('moveVertex_rough'  , isAreaInvariant, point);};
+
+FigureEditorByProximityHeuristic.prototype.pushEdge       = function (point, isAreaInvariant) {this.withAreaInvarianceIf('pushEdge_rough'      , isAreaInvariant, point);};
+FigureEditorByProximityHeuristic.prototype.pushnormalEdge = function (point, isAreaInvariant) {this.withAreaInvarianceIf('pushnormalEdge_rough', isAreaInvariant, point);};
+FigureEditorByProximityHeuristic.prototype.spanEdge = function (point, drgV, isAreaInvariant) {this.withAreaInvarianceIf('spanEdge_rough', isAreaInvariant, point, drgV);};
+
+
+
+FigureEditorByProximityHeuristic.prototype.withAreaInvarianceIf = function (command, isAreaInvariant, point, optArg)
+{
+	this[isAreaInvariant ? 'withAreaInvariance' : 'plainExec'](command, point, optArg);
+};
+
+FigureEditorByProximityHeuristic.prototype.withAreaInvariance = function (command, point, optArg)
 {
 	const areaBefore = this.figure.area();
-	this[command](point);
+	this.plainExec(command, point, optArg);
 	const areaAfter = this.figure.area();
 	const q = Math.sqrt(areaAfter/areaBefore);
 	this.figure.doScale(1/q);
 };
 
+FigureEditorByProximityHeuristic.prototype.plainExec = function (command, point, optArg)
+{
+	typeof optArg != undefined ? this[command](point, optArg)
+	                           : this[command](point);
+};
 
-FigureEditorByProximityHeuristic.prototype.addVertex    = function (point) {this.withAreaInvariance('addVertex_rough'   , point);};
-FigureEditorByProximityHeuristic.prototype.deleteVertex = function (point) {this.withAreaInvariance('deleteVertex_rough', point);};
-FigureEditorByProximityHeuristic.prototype.moveVertex   = function (point) {this.withAreaInvariance('moveVertex_rough'  , point);};
+
+
+FigureEditorByProximityHeuristic.prototype.addVertex_rough    = function (point) {this.figure.vertices = addVertex   (this.figure.vertices, point);};
+FigureEditorByProximityHeuristic.prototype.deleteVertex_rough = function (point) {this.figure.vertices = deleteVertex(this.figure.vertices, point);};
+FigureEditorByProximityHeuristic.prototype.moveVertex_rough   = function (point) {this.figure.vertices = moveVertex  (this.figure.vertices, point);};
+
+FigureEditorByProximityHeuristic.prototype.pushEdge_rough       = function (point)             {this.figure.vertices = pushEdge      (this.figure.vertices, point            );};
+FigureEditorByProximityHeuristic.prototype.pushnormalEdge_rough = function (point)             {this.figure.vertices = pushnormalEdge(this.figure.vertices, point            );};
+FigureEditorByProximityHeuristic.prototype.spanEdge_rough       = function (point, dragVector) {this.figure.vertices = spanEdge      (this.figure.vertices, point, dragVector);};
